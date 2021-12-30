f1ea8516759aacbf8f9d13094d4ab9c578511abb
Nahuel <Soccernahu@gmail.com>
2021-12-30 16:42:56 -0300

    FINAL

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var cont = 0
 var nivel = 1;
-var puntos;
-var resp_desord = [0, 1, 2, 3]
-var respuestas_reordenadas = [];
+var puntos = 0;
 let resp0 = document.getElementsByName('r0');
 let resp1 = document.getElementsByName('r1');
 let resp2 = document.getElementsByName('r2');
 let resp3 = document.getElementsByName('r3');
-let correcta=[];
+let correcta = [];
 function responder(x) {
 
 
@@ -57,7 +55,7 @@ function desordenar_lista(array) {
     }
 }
 
-
+// FUNCION MUESTRA LAS RESPUESTAS Y LAS REORDENA
 function mostrarDatos(array) {
     let i_aleatorio = Math.floor(Math.random() * array.length)
     // indice aleatorio para seleccionar pregunta
@@ -65,44 +63,77 @@ function mostrarDatos(array) {
     // seleccion de respuestas segun indice aleatorio   
     document.getElementById('pregunta').innerText = array[i_aleatorio].question;
     // imprimo pregunta
-    console.log(respuestas[0].text)
     correcta = respuestas[0].text;
     // agrego la respuesta correcta para verificar despues
     // imprimo 4 de las 5 respuestas      
     // llamo funcion de orden para las respuestas. 
     desordenar_lista(respuestas);
-    
-
+    niveles()
 }
-// FUNCION PARA VERIFICAR NIVEL SEGUN PUNTAJE DE RESPUESTAS 
+// FUNCION PARA VERIFICAR NIVEL SEGUN PUNTAJE ALCANZADO COMIENZA EN 1
 function niveles() {
-    if (puntos >= 0 && puntos <= 200) {
-        return nivel = 1;
+    if (puntos >= 0 && puntos <= 199) {
+        nivel = 1;
     }
-    if (puntos >= 201 && puntos <= 400) {
-        return nivel = 2;
+    if (puntos >= 200 && puntos <= 399) {
+        nivel = 2;
     }
-    if (puntos >= 401 && puntos <= 600) {
-        return nivel = 3;
+    if (puntos >= 400 && puntos <= 599) {
+        nivel = 3;
     }
-    if (puntos >= 601 && puntos <= 800) {
-        return nivel = 4;
+    if (puntos >= 600 && puntos <= 799) {
+        nivel = 4;
     }
-    if (puntos >= 801 && puntos <= 1500) {
-        return nivel = 5;
+    if (puntos >= 800 && puntos <= 1500) {
+        nivel = 5;
     }
+    document.getElementById('nivel').innerHTML = `<p>${nivel}</p>`
 }
+function infoDanger() {
+    document.getElementById('info-modal').innerHTML = `<div><br><img src="./img/incorrecto.png" alt=""><br><hr><h5> Quedaste en el nivel ${nivel} con esta respuesta incorrecta</h5><br><p>Responde 2 preguntas correctas por nivel</h5></div>`
+}
+function infoSuccess() {
+    document.getElementById('info-modal').innerHTML = `<div><br><img src="./img/correcto.png" alt=""><br><hr><h5> Quedaste en el Nivel:${nivel} con esta respuesta correcta</h5><br><p>Continúa así hasta llegar al nivel 5</h5><br><p></p></div>`
+}
+// verifica al clickear si la respuesta clickeada es correcta
 function verificarResp(x) {
-    console.log(x)
-    console.log(correcta)
-    if (x == correcta) {
-        alert('RESPUESTA CORRECTA')
+    if (x.value == correcta) {
+        document.getElementById(x.id).className = "but btn btn-success"
+
         if (nivel != 5) {
+            // en caso de serlo y estar en niveles diferentes a 5 suma +100 puntos por pregunta resuelta
             puntos += 100;
         }
+        else {
+            // en caso de estar en lvl5 con 2 preguntas ya completas los 1500 puntos necesarios para ganar. 
+            puntos += 350;
+        }
+        niveles()
+        showPoint()
+        infoSuccess();
     }
     else {
-        return alert('RESPUESTA INCORRECTA, intenta denuevo')
+        infoDanger(x)
+        document.getElementById(x.id).className = "but btn btn-danger"
+        // en caso contrario solo te avisa que no es la correcta y se vuelve a cargar la page sin modificar el puntaje.
+    }
+}
+
+function ganar(){
+    if(puntos == 1500){
+    alert('FELICIDADES, GANASTE LA TRIVIA!');
+    localStorage.removeItem('Trivia');
+    puntos=0;
+    nivel=1;
+    }
+    
+}
+function reordenar() {
+    let res = document.getElementsByClassName('but');
+    for (let i = 0; i < res.length; i++) {
+        let x = res[i];
+        document.getElementById(x.id).className = 'but btn btn-outline-info';
+
     }
 }
 // funcion para reiniciar el juego a 0
@@ -111,37 +142,77 @@ function reiniciar() {
     puntos = 0;
     nivel = 1;
 };
-console.log(
-    document.getElementById('resp0').value)
+// funcion para mostrar el nivel y los puntajes
+function showPoint() {
+    document.getElementById('punto').innerHTML = `<p>${puntos}</p>`
+}
+// funcion para finalizar juego
+function fin() {
+    localStorage.setItem('Trivia', [puntos, nivel]);
+}
 
 // EVENTOS DOM 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(NIVELES + nivel + '.json').then(function (resultado) {
         if (resultado.status === 'ok') {
-            let result = resultado.data;
-            niveles();
+            var result = resultado.data;
             mostrarDatos(result);
-            console.log(result)
+            niveles()
+            showPoint()
         }
     })
-    document.getElementById('resp0').addEventListener('click', function(e){
-        let value0=document.getElementById('resp0').value;
+    document.getElementById('resp0').addEventListener('click', function (e) {
+        let value0 = document.getElementById('resp0');
         verificarResp(value0);
-        console.log('value0')
     })
-    document.getElementById('resp1').addEventListener('click', function(e){
-        let value1=document.getElementById('resp1').value;
+    document.getElementById('resp1').addEventListener('click', function (e) {
+        let value1 = document.getElementById('resp1');
         verificarResp(value1);
-        console.log('value1')
     })
-    document.getElementById('resp2').addEventListener('click', function(e){
-        let value2=document.getElementById('resp2').value;
+    document.getElementById('resp2').addEventListener('click', function (e) {
+        let value2 = document.getElementById('resp2');
         verificarResp(value2);
-        console.log('value2')
     })
-    document.getElementById('resp3').addEventListener('click', function(e){
-        let value3=document.getElementById('resp3').value;
-        verificarResp(value3);
-        console.log('value3')
+    document.getElementById('resp3').addEventListener('click', function (e) {
+        let value3 = document.getElementById('resp3');
+        setTimeout(verificarResp(value3), 2000);
     })
+    document.getElementById('continuar').addEventListener('click', function (e) {
+        niveles();
+        showPoint()
+        reordenar()
+        getJSONData(NIVELES + nivel + '.json').then(function (resultado) {
+            if (resultado.status === 'ok') {
+                var result = resultado.data;
+                mostrarDatos(result);
+            }
+        })
+        ganar()
+    })
+    document.getElementById('recargar').addEventListener('click', function (e) {
+        reordenar();
+        getJSONData(NIVELES + nivel + '.json').then(function (resultado) {
+            if (resultado.status === 'ok') {
+                var result = resultado.data;
+                mostrarDatos(result);
+            }
+        })
+                ganar()
+
+    })
+    document.getElementById('comenzar').addEventListener('click', function (e) {
+        reiniciar()
+        reordenar()
+        getJSONData(NIVELES + nivel + '.json').then(function (resultado) {
+            if (resultado.status === 'ok') {
+                var result = resultado.data;
+                mostrarDatos(result);
+            }
+        })
+    })
+    document.getElementById('fin').addEventListener('click', function (e) {
+        fin()
+    })
+
+
 })
